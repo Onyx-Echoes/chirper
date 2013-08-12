f57f8b7924544beb6bbd81415aaf1a82d9ffbd69
Onyx-Echoes <dfz3@live.com>
2013-08-11 19:55:33 -0500

    Initial commit with Rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chirper::Application.config.secret_key_base = '825121186de0a690d83c18e35d3611fafebb484e8b2fa5ff2c1514e51476938dbac433809faa29a2f029290bfb8600f327e23779c6029c23e57aa64c71a638b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chirper::Application.config.session_store :cookie_store, key: '_chirper_session'
